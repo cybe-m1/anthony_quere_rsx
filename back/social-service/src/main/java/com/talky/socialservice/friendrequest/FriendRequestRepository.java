c83cfba90dc84e28cbcbc253da971b6bf40846f6
anthony <quereantho@gmail.com>
2022-05-29 10:56:14 +0200

    :bug: Fix friend request creation

--- back/social-service/src/main/java/com/talky/socialservice/friendrequest/FriendRequestRepository.java
+++ back/social-service/src/main/java/com/talky/socialservice/friendrequest/FriendRequestRepository.java
@@ -2,6 +2,7 @@ package com.talky.socialservice.friendrequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.Set;
@@ -18,6 +19,6 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, UU
   @Query("SELECT fr from FriendRequest fr where " +
     "((fr.sender = :user1 and fr.recipient = :user2) or" +
     "(fr.sender = :user2 and fr.recipient = :user1)) and fr.status = 'PENDING'")
-  Optional<FriendRequest> findFriendRequestByUsers(UUID user1, UUID user2);
+  Optional<FriendRequest> findFriendRequestByUsers(@Param("user1") UUID user1, @Param("user2") UUID user2);
 
 }
