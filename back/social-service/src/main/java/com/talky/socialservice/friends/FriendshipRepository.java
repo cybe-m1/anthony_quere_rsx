dcec026cd9b220b0bbb7c886ef66d843cd4c7334
anthony <quereantho@gmail.com>
2022-06-06 15:30:47 +0200

    :bug: Add missing @Param for jpa query

--- back/social-service/src/main/java/com/talky/socialservice/friends/FriendshipRepository.java
+++ back/social-service/src/main/java/com/talky/socialservice/friends/FriendshipRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,5 @@ public interface FriendshipRepository extends JpaRepository<Friendship, UUID> {
   Page<Friendship> findByFriendAOrFriendB(UUID friendA, UUID friendB, Pageable pageable);
 
   @Query("select f from Friendship f WHERE (f.friendA = :friendA AND f.friendB = :friendB) OR (f.friendA = :friendB AND f.friendB = :friendA)")
-  Optional<Friendship> findByFriends(UUID friendA, UUID friendB);
+  Optional<Friendship> findByFriends(@Param("friendA") UUID friendA, @Param("friendB") UUID friendB);
 }
